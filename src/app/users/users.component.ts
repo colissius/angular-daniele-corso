f0ea994e3a9ea9b8e85cd997b7a2f1d01ad021dc
colissius <daguannodaniele84@gmail.com>
2022-03-15 21:14:57 +0100

    lezione 15

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  title = 'Users';
+  users = [
+    {
+      name: 'name',
+      lastName: 'lastName',
+      fiscalCode: 'fiscalCode',
+      email: 'email',
+      phone: 'phone',
+      province: 'province',
+      age: 'age',
+    },
+    {
+      name: 'name1',
+      lastName: 'lastName1',
+      fiscalCode: 'fiscalCode1',
+      email: 'email1',
+      phone: 'phone1',
+      province: 'province1',
+      age: 'age1',
+    },
+    {
+      name: 'name2',
+      lastName: 'lastName2',
+      fiscalCode: 'fiscalCode2',
+      email: 'email2',
+      phone: 'phone2',
+      province: 'province2',
+      age: 'age2',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
