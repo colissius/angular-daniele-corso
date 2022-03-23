2aced19f97892899d16926ce336c1b4322fb4d6a
colissius <daguannodaniele84@gmail.com>
2022-03-23 21:09:01 +0100

    Lezione 20

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
+  selector: 'tr[app-user]',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
+  @Input('user-data') user;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
