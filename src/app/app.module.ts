4d4c8392b13c2bb5c46df67a33fbf8161660c962
colissius <daguannodaniele84@gmail.com>
2022-04-04 20:36:40 +0200

    Lezione 24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, UsersComponent, UserComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
